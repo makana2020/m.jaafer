d9ec581559e7e0e78d02ca6393f36fac80b57e28
makana2020 <124792762+makana2020@users.noreply.github.com>
2023-03-01 18:08:26 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+
+let scc1=document.getElementById('sccc1')
+let scc2=document.getElementById('sccc2')
+let scc3=document.getElementById('sccc3')
+let scc4=document.getElementById('sccc4')
+
+onscroll=function num(){
+    if(this.scrollY>2888){
+    scc1.style=` width: 90%;
+    animation: ph 3s ;`
+    scc2.style=` width: 80%;
+    animation: ill 3s;`
+    scc3.style=` width: 65%;
+    animation: bl 3s;`
+    scc4.style=` width: 50%;
+    animation: in 3s;`
+    }
+}
+
+
+
+
+
+
